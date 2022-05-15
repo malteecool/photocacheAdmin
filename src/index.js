98df0a134795b1af8475d0531c6cc9784d6a0e29
malte lindgren <malteecool@hotmail.com>
2022-05-15 19:07:36 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ async function onLoad() {
     const dbRef = ref(db);
     get(child(dbRef, 'cache')).then((snapshot) => {
         if (snapshot.exists()) {
-            for(let x in snapshot.val()) {
+            for (let x in snapshot.val()) {
                 var child = snapshot.child(x);
                 console.log(child.val());
                 fillCacheData(child.val());
@@ -35,28 +35,56 @@ async function onLoad() {
 
 
 function fillCacheData(cache) {
-
     const cardDiv = document.createElement("div");
+    cardDiv.setAttribute("class", "card");
 
-    cardDiv.appendChild(buildCardImage(cache.imageID));
-    cardDiv.appendChild(buildCardTitle(cache.title));
-    cardDiv.appendChild(buildCardDescription(cache.desc));
-    cardDiv.setAttribute("onclick", "location.href='details.html'");
-    cardDiv.setAttribute("class", "cardDivInner");
+    const cardInnerDiv = document.createElement("div");
+    cardInnerDiv.setAttribute("class", "inner-div");
 
-    const cardDivOuter = document.createElement("div");
-    cardDivOuter.setAttribute("class", "card");
-    cardDivOuter.appendChild(cardDiv);
-    cardDivOuter.appendChild(buildButtonLine());
+    cardInnerDiv.appendChild(buildCardImage(cache.ImageId));
+    cardInnerDiv.appendChild(buildCardTitle(cache.title));
+    cardInnerDiv.appendChild(buildCardDescription(cache.desc));
 
-    const contentDiv = document.getElementById("cards").appendChild(cardDivOuter);
+    cardDiv.appendChild(cardInnerDiv);
+
+    // buttonline
+
+    // div button line
+
+    const buttonLineDiv = document.createElement("div");
+    buttonLineDiv.setAttribute("class", "button-line");
+
+    const div1 = document.createElement("div");
+    const link1 = document.createElement("a");
+    link1.setAttribute("href", "#");
+    const img1 = document.createElement("img");
+    img1.setAttribute("src", "icons/edit.svg");
+    link1.appendChild(img1);
+    div1.appendChild(link1);
+    
+    const div2 = document.createElement("div");
+    const link2 = document.createElement("a");
+    link2.setAttribute("href", "#");
+    const img2 = document.createElement("img");
+    img2.setAttribute("src", "icons/trash-2.svg");
+    link2.appendChild(img2);
+    div2.appendChild(link2);
+
+    buttonLineDiv.appendChild(div1);
+    buttonLineDiv.appendChild(div2);
+
+    cardDiv.appendChild(buttonLineDiv);
+
+    document.getElementById("cards").appendChild(cardDiv);
+
+
 }
 
 function buildCardImage(ImageId) {
     // To be updated with the correct image.
     const cardImage = document.createElement("img");
     cardImage.setAttribute("class", "card-image");
-    cardImage.setAttribute("src", "placeholder.png");
+    cardImage.setAttribute("src", "icons/placeholder.png");
     cardImage.setAttribute("width", "200px");
     cardImage.setAttribute("height", "200px");
     return cardImage;
@@ -77,18 +105,31 @@ function buildCardDescription(desc) {
 }
 
 function buildButtonLine() {
-    // Update for a pic later
-
     const buttonLineDiv = document.createElement("div");
     buttonLineDiv.setAttribute("class", "button-line");
 
-    const cardButtonRemove = document.createElement("button");
-    cardButtonRemove.appendChild(document.createTextNode("Remove"));
-    cardButtonRemove.setAttribute("onclick", "#");
+    const editButton = document.createElement("a");
+    editButton.setAttribute("href", "details.html");
+    const editIcon = document.createElement("img");
+    editIcon.setAttribute("src", "icons/edit.svg");
+    editIcon.setAttribute("alt", "Edit");
+    editButton.appendChild(editIcon);
 
-    buttonLineDiv.appendChild(cardButtonRemove);
+    const removeButton = document.createElement("a");
+    removeButton.setAttribute("href", "#");
+    const removeIcon = document.createElement("img");
+    removeIcon.setAttribute("src", "icons/trash-2.svg");
+    removeIcon.setAttribute("alt", "Remove");
+    removeButton.appendChild(removeIcon);
+
+
+    buttonLineDiv.appendChild(editButton);
+    buttonLineDiv.appendChild(removeButton);
 
     return buttonLineDiv;
 }
+
+
+
 
 onLoad();
